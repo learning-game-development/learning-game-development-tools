bb19623ae751bcf659bb42b4f90d6e6b428fcf4d
Quintin Henn <quintin.henn@gmail.com>
2023-02-21 06:40:15 +0100

    Completed introduction to Tiled

--- learning-tiled/Introduction-to-tiled/README.md
+++ learning-tiled/Introduction-to-tiled/README.md
@@ -1,3 +1,5 @@
 # Getting Started with Tiled
 
 :document: [Tiled Documentation](https://doc.mapeditor.org/en/stable/)
+
+!![Example tiled map](example.png)
--- learning-tiled/Introduction-to-tiled/tilesheets/tmw_desert_spacing.tsx
+++ learning-tiled/Introduction-to-tiled/tilesheets/tmw_desert_spacing.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="tmw_desert_spacing" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="../../../zenva-courses/tiled-master/examples/tmw_desert_spacing.png" width="265" height="199"/>
+<tileset version="1.9" tiledversion="1.9.2" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="../../../../../../Users/Quintin.Henn/AppData/Roaming/itch/apps/tiled/examples/tmw_desert_spacing.png" width="265" height="199"/>
 </tileset>
