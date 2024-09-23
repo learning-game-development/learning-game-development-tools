46b4702ed676557bb4a8f4e31f56034a062330ce
Quintin Henn <quintin.henn@gmail.com>
2024-09-23 11:46:24 +0200

    Completed The Basics

---
+++ learning-tiled/tiled-map-editor-tutorial-series/tilesheets/overworld_tileset_grass.tsx
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="overworld_tileset_grass" tilewidth="16" tileheight="16" tilecount="252" columns="12">
+ <image source="../../../../../../Users/Quintin.Henn/AppData/Roaming/itch/apps/overworld-tileset-grass-biome/TilesetGrass/overworld_tileset_grass.png" width="192" height="336"/>
+ <wangsets>
+  <wangset name="World" type="corner" tile="-1">
+   <wangcolor name="Forest" color="#ff0000" tile="70" probability="1"/>
+   <wangcolor name="Grass" color="#00ff00" tile="0" probability="1"/>
+   <wangcolor name="Water" color="#0000ff" tile="61" probability="1"/>
+   <wangtile tileid="0" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="48" wangid="0,2,0,3,0,2,0,2"/>
+   <wangtile tileid="49" wangid="0,2,0,3,0,3,0,2"/>
+   <wangtile tileid="50" wangid="0,2,0,2,0,3,0,2"/>
+   <wangtile tileid="57" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="58" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="59" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="60" wangid="0,3,0,3,0,2,0,2"/>
+   <wangtile tileid="61" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="62" wangid="0,2,0,2,0,3,0,3"/>
+   <wangtile tileid="69" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="70" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="71" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="72" wangid="0,3,0,2,0,2,0,2"/>
+   <wangtile tileid="73" wangid="0,3,0,2,0,2,0,3"/>
+   <wangtile tileid="74" wangid="0,2,0,2,0,2,0,3"/>
+   <wangtile tileid="81" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="82" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="83" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="84" wangid="0,3,0,2,0,3,0,3"/>
+   <wangtile tileid="85" wangid="0,3,0,3,0,2,0,3"/>
+   <wangtile tileid="93" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="94" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="96" wangid="0,2,0,3,0,3,0,3"/>
+   <wangtile tileid="97" wangid="0,3,0,3,0,3,0,2"/>
+   <wangtile tileid="105" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="106" wangid="0,1,0,1,0,1,0,2"/>
+  </wangset>
+  <wangset name="Roads" type="edge" tile="-1">
+   <wangcolor name="Dirt" color="#ff0000" tile="30" probability="1"/>
+   <wangcolor name="Stone" color="#00ff00" tile="34" probability="1"/>
+   <wangtile tileid="5" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="6" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="7" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="9" wangid="0,0,2,0,0,0,1,0"/>
+   <wangtile tileid="10" wangid="0,0,2,0,0,0,2,0"/>
+   <wangtile tileid="11" wangid="0,0,1,0,0,0,2,0"/>
+   <wangtile tileid="16" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="17" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="19" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="20" wangid="1,0,0,0,2,0,0,0"/>
+   <wangtile tileid="21" wangid="0,0,2,0,2,0,0,0"/>
+   <wangtile tileid="22" wangid="0,0,2,0,2,0,2,0"/>
+   <wangtile tileid="23" wangid="0,0,0,0,2,0,2,0"/>
+   <wangtile tileid="28" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="29" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="30" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="31" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="32" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="33" wangid="2,0,2,0,2,0,0,0"/>
+   <wangtile tileid="34" wangid="2,0,2,0,2,0,2,0"/>
+   <wangtile tileid="35" wangid="2,0,0,0,2,0,2,0"/>
+   <wangtile tileid="40" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="41" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="42" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="43" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="44" wangid="2,0,0,0,1,0,0,0"/>
+   <wangtile tileid="45" wangid="2,0,2,0,0,0,0,0"/>
+   <wangtile tileid="46" wangid="2,0,2,0,0,0,2,0"/>
+   <wangtile tileid="47" wangid="2,0,0,0,0,0,2,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
