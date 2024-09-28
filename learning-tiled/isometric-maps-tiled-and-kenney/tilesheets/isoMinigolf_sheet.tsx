dfce225cc8fae658a6b7a315841439d91d9baa83
Quintin Henn <quintin.henn@gmail.com>
2024-09-28 14:51:53 +0200

    Completed isometric-maps-tiled-and-kenney

---
+++ learning-tiled/isometric-maps-tiled-and-kenney/tilesheets/isoMinigolf_sheet.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="isoMinigolf_sheet" tilewidth="180" tileheight="256" tilecount="165" columns="15">
+ <tileoffset x="0" y="75"/>
+ <image source="isoMinigolf_sheet.png" width="2700" height="2816"/>
+</tileset>
